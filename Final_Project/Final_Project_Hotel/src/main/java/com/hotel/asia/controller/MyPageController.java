97934af0e34c2f4e5b00da254e90f198fe908cdb
idejrdud <idejrdud@gmail.com>
2022-09-26 17:54:33 +0900

    [ADD] mypage.jsp -> 마이페이지 예약정보확인

---
+++ Final_Project/Final_Project_Hotel/src/main/java/com/hotel/asia/controller/MyPageController.java
@@ -0,0 +1,16 @@
+package com.hotel.asia.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyPageController {
+	private static final Logger log = LoggerFactory.getLogger(TestController.class);
+	@GetMapping("/mypage")
+	public String main() {
+		log.info("ggggggggggggggggggg");
+		return "mypage/mypage_r";
+	}
+}
