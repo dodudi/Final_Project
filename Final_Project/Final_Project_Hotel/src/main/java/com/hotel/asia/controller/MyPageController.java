add0977d4c77aaf17af0a33cedbc51f2c7398499
idejrdud <idejrdud@gmail.com>
2022-09-27 15:16:13 +0900

    [UPDATE] My page controller update

--- Final_Project/Final_Project_Hotel/src/main/java/com/hotel/asia/controller/MyPageController.java
+++ Final_Project/Final_Project_Hotel/src/main/java/com/hotel/asia/controller/MyPageController.java
@@ -4,23 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyPageController {
 	private static final Logger log = LoggerFactory.getLogger(MyPageController.class);
+
 	@GetMapping("/mypage/reserve")
-	public String Reserve () {
+	public String reserve() {
 		return "mypage/mypage_reserve_check";
 	}
-	
+
 	@GetMapping("/mypage/question")
-	public String Question() {
+	public String question() {
 		return "mypage/mypage_question_check";
 	}
-	
-	@GetMapping("/member")
-	public String Member() {
+
+	@GetMapping("/mypage/member")
+	public String member() {
 		return "mypage/mypage_member_check";
+	}
+
+	@GetMapping("/mypage/coupon")
+	public String coupon() {
+		return "mypage/mypage_coupon";
+	}
+
+	@GetMapping("/mypage/wishBoard")
+	public String wishBoard() {
+		return "mypage/mypage_wishboard";
 	}
 }
