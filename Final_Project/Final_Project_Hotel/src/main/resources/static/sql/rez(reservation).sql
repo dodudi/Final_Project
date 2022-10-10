b7fde28b9a192640324b0d2de05e8d1e6464a1ab
cjm44 <cjm44@192.168.200.137>
2022-10-10 22:06:09 +0900

    rez파일 에러 테스트

--- Final_Project/Final_Project_Hotel/src/main/resources/static/sql/rez(reservation).sql
+++ Final_Project/Final_Project_Hotel/src/main/resources/static/sql/rez(reservation).sql
@@ -1,12 +1,16 @@
+--예약 테스트떄 delete 필요
 create table REZ (
-	REZ_ID 		 NUMBER 	  primary key,
-	ROOM_ID 	 NUMBER 	  references ROOM(ROOM_ID),
-	MEM_ID 		 VARCHAR2(30) references MEMBER(MEM_ID),
-	REZ_CHECKIN  DATE,
-	REZ_CHECKOUT DATE,
-	REZ_ADULT 	 NUMBER,
-	REZ_CHILD 	 NUMBER
-)
+   REZ_ID        NUMBER      primary key,
+   ROOM_ID     NUMBER      references ROOM(ROOM_ID),
+   MEM_ID        VARCHAR2(30) references MEMBER(MEM_ID),
+   REZ_CHECKIN  DATE,
+   REZ_CHECKOUT DATE,
+   REZ_ADULT     NUMBER,
+   REZ_CHILD     NUMBER
+);
 
-select * from rez
-delete from rez
+
+select * from rez;
+
+commit;
+delete from rez where rez_id = 5
