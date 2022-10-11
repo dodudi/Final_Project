073cc803aaa0261f86b1c0399e272f875db432cc
idejrdud <idejrdud@gmail.com>
2022-10-11 17:46:51 +0900

    [UPDATE] 옵션 날짜 구하기, 옵션 별 가격 측정 기능 추가

--- Final_Project/Final_Project_Hotel/src/main/java/com/hotel/asia/service/MyPageService.java
+++ Final_Project/Final_Project_Hotel/src/main/java/com/hotel/asia/service/MyPageService.java
@@ -1,19 +1,48 @@
 package com.hotel.asia.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.hotel.asia.controller.MyPageController;
+import com.hotel.asia.dto.Option;
 import com.hotel.asia.dto.OptionReservation;
 import com.hotel.asia.dto.Payment;
 import com.hotel.asia.dto.Rez;
+import com.hotel.asia.mybatis.mapper.MyPageMapper;
 
 @Service
 public interface MyPageService {
-	//객실정보 가져오기
+
+	// 객실정보 가져오기
 	public Rez getRezData(String mem_id);
-	//옵션선택정보 가져오기
+
+	// 옵션선택정보 가져오기
 	public List<OptionReservation> getOptRezData(String mem_id);
-	//결제정보 가져오기
-	public Payment getPayment(String mem_id);
+	// 결제정보 가져오기
+	public Payment getPayment(String mem_id) ;
+	// 멤버 아이디에 대한 옵션 가져오기
+	public List<OptionReservation> getOptRezData(String mem_id, int opt_id);
+
+	// 날짜에 대한 옵션 가져오기
+	public List<OptionReservation> getOptRezData(String mem_id, String date, int opt_id);
+	
+
+	// 체크아웃 날짜 - 체크인 날짜 -> 몇일 ex) 2022-10-10 ~ 2022-10-13 -> 3
+	public long getDateSub(String start, String end) ;
+
+
+	// 옵션 날짜 구하기 -> ex) 2022-10-10 3일 -> 2022-10-10~2022-10-13까지의 날짜를 배열로 반환
+	public List<String> calcBreakFastDate(String checkIn, long subDate);
+	
+	public Map<String, Map<String, Integer>> getOptPrice(List<OptionReservation> opts, List<Option> optInfo,  int opt_id);
 }
