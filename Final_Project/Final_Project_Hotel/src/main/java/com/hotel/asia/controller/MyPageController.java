a3022edbd4ec719d7ba6359b8691e0f4bbe365fd
idejrdud <idejrdud@gmail.com>
2022-10-12 17:37:36 +0900

    [ADD] 객실 수정 체크 기능 추가

--- Final_Project/Final_Project_Hotel/src/main/java/com/hotel/asia/controller/MyPageController.java
+++ Final_Project/Final_Project_Hotel/src/main/java/com/hotel/asia/controller/MyPageController.java
@@ -49,6 +49,7 @@ public class MyPageController {
 	private OptionService optionService;
 	@Autowired
 	private RoomService roomService;
+
 	// 객실예약확인 페이지
 	@GetMapping("/mypage/reserve")
 	public String reserve(Model model, HttpSession session) {
@@ -76,7 +77,7 @@ public class MyPageController {
 		return "mypage/mypage_reserve_check";
 	}
 
-	// 날짜 별로 객실정보 가져오기
+	// 날짜 별로 객실정보 가져오기 Ajax
 	@ResponseBody
 	@PostMapping(value = "/mypage/dateCheck")
 	public HashMap<String, OptionReservation> rezCheckDate(Model model, @RequestBody HashMap<String, Object> date) {
@@ -106,7 +107,7 @@ public class MyPageController {
 		return resultData;
 	}
 
-	// 테스트 날짜 입력 폼으로 이동
+	// 테스트 날짜 입력 폼으로 이동 /reservation/testRezModify.jsp
 	@PostMapping(value = "/mypage/testRezModify")
 	public String testRezModify(Model model, HttpSession session) {
 		// String mem_id = session.getAttribute("id").toString();
@@ -122,7 +123,7 @@ public class MyPageController {
 		return "reservation/testRezModify";
 	}
 
-	// 객실예약 수정 페이지로 이동
+	// 객실예약 수정 페이지로 이동 /option/optionModifyForm.jsp
 	@PostMapping(value = "/mypage/optionModifyForm")
 	public String rezModifyForm(Model model, HttpSession session, Rez rezData) {
 		log.info(rezData.getROOM_ID() + "");
@@ -141,81 +142,80 @@ public class MyPageController {
 		model.addAttribute("optionList", optionList);
 		return "option/optionModifyForm";
 	}
-	
-	//객실 수정 체크
-	@PostMapping(value="/mypage/optCheck")
-	public ModelAndView rezCheck(Rez rez, int nights, ModelAndView mv, HttpServletRequest request,HttpSession session) {
-		
+
+	// 객실 수정 체크 /option/mypage_optCheck.jsp
+	@PostMapping(value = "/mypage/optCheck")
+	public ModelAndView rezCheck(Rez rez, int nights, ModelAndView mv, HttpServletRequest request,
+			HttpSession session) {
 
 		// 체크인 날짜 ~ 체크아웃 날짜
 		List<String> dateList = new ArrayList<String>();
-		for(int i = 1; i<= nights+1 ; i++) {
+		for (int i = 1; i <= nights + 1; i++) {
 			dateList.add(request.getParameter("spDate" + i));
 		}
-		
+
 		// 옵션 리스트 - 옵션별 가격 구하기
 		List<Option> optionInfo = optionService.getOptionList();
-		for(Option oi : optionInfo) {
+		for (Option oi : optionInfo) {
 			log.info("* " + oi.getOPTION_NAME());
 			log.info("성인가격 : " + oi.getOPTION_DEFAULT_PRICE());
 			log.info("아동가격 : " + oi.getOPTION_CHILD_PRICE());
 		}
-		
+
 		// 옵션별 총금액
 		Map<String, Integer> optionPrice = new HashMap<String, Integer>();
 		optionPrice.put("bfTotal", Integer.parseInt(request.getParameter("bfTotal").replaceAll(",", "")));
 		optionPrice.put("dnTotal", Integer.parseInt(request.getParameter("dnTotal").replaceAll(",", "")));
 		optionPrice.put("spTotal", Integer.parseInt(request.getParameter("spTotal").replaceAll(",", "")));
-		
+
 		log.info("객실정보");
 		// 객실 정보
 		Room room = roomService.getRoomDetail(rez.getROOM_ID()); // 선택한 객실 정보 구하기 (조회되는 행은 1개)
 		log.info("** 선택된 객실 가격 =>" + room.getROOM_PRICE());
-		
-		
+
 		log.info("조식");
 		// 조식
-		List<Map<String, Map<String, Integer>>> optList = new ArrayList<Map<String, Map<String, Integer>>>(); 
-		for(int i = 1; i<= nights; i++) {
+		List<Map<String, Map<String, Integer>>> optList = new ArrayList<Map<String, Map<String, Integer>>>();
+		for (int i = 1; i <= nights; i++) {
 			Map<String, Integer> bfPeople = new HashMap<String, Integer>();
 			Map<String, Map<String, Integer>> bfDate = new HashMap<String, Map<String, Integer>>();
-			bfPeople.put("bfAdult", Integer.parseInt( request.getParameter("bfAdult" + i)));
-			bfPeople.put("bfChild", Integer.parseInt( request.getParameter("bfChild" + i)));
+			bfPeople.put("bfAdult", Integer.parseInt(request.getParameter("bfAdult" + i)));
+			bfPeople.put("bfChild", Integer.parseInt(request.getParameter("bfChild" + i)));
 			bfDate.put(request.getParameter("bfDate" + i), bfPeople);
 			optList.add(bfDate);
 		}
 
 		log.info("디너");
 		// 디너
-		for(int i = 1; i<= nights; i++) {
+		for (int i = 1; i <= nights; i++) {
 			Map<String, Integer> dnPeople = new HashMap<String, Integer>();
 			Map<String, Map<String, Integer>> dnDate = new HashMap<String, Map<String, Integer>>();
-			dnPeople.put("dnAdult", Integer.parseInt( request.getParameter("dnAdult" + i)));
-			dnPeople.put("dnChild", Integer.parseInt( request.getParameter("dnChild" + i)));
+			dnPeople.put("dnAdult", Integer.parseInt(request.getParameter("dnAdult" + i)));
+			dnPeople.put("dnChild", Integer.parseInt(request.getParameter("dnChild" + i)));
 			dnDate.put(request.getParameter("dnDate" + i), dnPeople);
 			optList.add(dnDate);
 		}
-		
+
 		log.info("수영장");
 		// 수영장
-		for(int i = 1; i<= nights+1 ; i++) {
+		for (int i = 1; i <= nights + 1; i++) {
 			Map<String, Integer> spPeople = new HashMap<String, Integer>();
 			Map<String, Map<String, Integer>> spDate = new HashMap<String, Map<String, Integer>>();
-			spPeople.put("spAdult", Integer.parseInt( request.getParameter("spAdult" + i)));
-			spPeople.put("spChild", Integer.parseInt( request.getParameter("spChild" + i)));
+			spPeople.put("spAdult", Integer.parseInt(request.getParameter("spAdult" + i)));
+			spPeople.put("spChild", Integer.parseInt(request.getParameter("spChild" + i)));
 			spDate.put(request.getParameter("spDate" + i), spPeople);
 			optList.add(spDate);
 		}
-		
+
 		log.info("정보확인");
 		// 담긴 정보 확인하기
-		for(Map<String, Map<String, Integer>> a : optList) {
-			for(String b : a.keySet()) {
-				//Map<String, Integer> c = a.get(b);
+		for (Map<String, Map<String, Integer>> a : optList) {
+			for (String b : a.keySet()) {
+				// Map<String, Integer> c = a.get(b);
 				log.info(b + "->" + a.get(b));
 			}
 		}
-		
+
 		mv.addObject("rez", rez); // 객실 예약 정보
 		mv.addObject("nights", nights); // 숙박일수
 		mv.addObject("dateList", dateList); // 날짜 리스트 (체크인 날짜 ~ 체크아웃 날짜)
@@ -224,36 +224,45 @@ public class MyPageController {
 		mv.addObject("optionPrice", optionPrice); // 옵션별 총금액
 		mv.addObject("room", room); // 객실 정보
 		mv.setViewName("option/mypage_optCheck");
-		
-		//이미 저장되어있는 데이터
+
+		// 이미 저장되어있는 데이터
 		String mem_id = session.getAttribute("id").toString();
-		 //기본 저장된 객실예약정보
-		Rez ori_rez = myPageService.getRezData(mem_id);		
-		//기존 저장된 체크인~체크아웃 날짜 일수
+		// 기본 저장된 객실예약정보
+		Rez ori_rez = myPageService.getRezData(mem_id);
+		// 기존 저장된 체크인~체크아웃 날짜 일수
 		long totalDate = myPageService.getDateSub(ori_rez.getREZ_CHECKOUT(), ori_rez.getREZ_CHECKIN());
-		//체크인 부터 체쿠아웃까지의 모든 날짜
-		List<String> ori_dates = myPageService.calcBreakFastDate(ori_rez.getREZ_CHECKIN(), totalDate+1);
-		
-		//모든 옵션의 데이터를 가져옵니다.
+		// 체크인 부터 체쿠아웃까지의 모든 날짜
+		List<String> ori_dates = myPageService.calcBreakFastDate(ori_rez.getREZ_CHECKIN(), totalDate + 1);
+
+		// 모든 옵션의 데이터를 가져옵니다.
 		List<OptionReservation> ori_break = myPageService.getOptRezData(mem_id, OptionId.BREAKFAST);
 		List<OptionReservation> ori_dinner = myPageService.getOptRezData(mem_id, OptionId.DINNER);
 		List<OptionReservation> ori_swim = myPageService.getOptRezData(mem_id, OptionId.SWIMMING);
-		
-		Map<String, Map<String, Integer>> ori_break_prices = myPageService.getOptPrice(ori_break, optionInfo, OptionId.BREAKFAST);
-		Map<String, Map<String, Integer>> ori_dinner_prices = myPageService.getOptPrice(ori_dinner, optionInfo, OptionId.DINNER);
-		Map<String, Map<String, Integer>> ori_swim_prices = myPageService.getOptPrice(ori_swim, optionInfo, OptionId.SWIMMING);
-		
+
+		Map<String, Map<String, Integer>> ori_break_prices = myPageService.getOptPrice(ori_break, optionInfo,
+				OptionId.BREAKFAST);
+		Map<String, Map<String, Integer>> ori_dinner_prices = myPageService.getOptPrice(ori_dinner, optionInfo,
+				OptionId.DINNER);
+		Map<String, Map<String, Integer>> ori_swim_prices = myPageService.getOptPrice(ori_swim, optionInfo,
+				OptionId.SWIMMING);
+
 		mv.addObject("ori_rez", ori_rez);
 		mv.addObject("ori_break", ori_break);
 		mv.addObject("ori_dinner", ori_dinner);
 		mv.addObject("ori_swim", ori_swim);
-		
+
 		mv.addObject("ori_break_prices", ori_break_prices);
 		mv.addObject("ori_dinner_prices", ori_dinner_prices);
 		mv.addObject("ori_swim_prices", ori_swim_prices);
 		mv.addObject("ori_dates", ori_dates);
 		return mv;
-		
+
+	}
+
+	// 객실 수정 처리! mypage/rezModify.jsp
+	@PostMapping(value = "/mypage/rezModify")
+	public String rezModify() {
+		return "mypage/rezModify";
 	}
 
 	// 질문게시판 페이지
